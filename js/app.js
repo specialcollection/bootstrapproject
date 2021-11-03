5de602425b0738e90b29ed26b756fe0212d580cb
Yan Naing Win <kyaw78836@gmail.com>
2021-11-03 21:36:02 +0630

    faqupdate

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
+// START FAQ
+
+const btnpluses = document.querySelectorAll(".btnplus")
+// console.log(btnplus);
+
+btnpluses.forEach(btnplus=>{
+    btnplus.addEventListener('click',()=>{
+        btnplus.classList.toggle('rotatebtn');
+    });
+});
+
+// END FAQ
+
 // start footer js
 var year = document.getElementById('year');
 year.textContent = new Date().getFullYear();
